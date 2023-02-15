c0505f0def32ff4ea4b7cc1d027811b3d150f535
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-15 14:11:21 +0300

    add list to navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
-
+    <div className='text-white'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <ul>
+        <li>Home</li>
+        <li>Company</li>
+        <li>Resources</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </div>
   )
 }
