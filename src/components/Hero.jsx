6d5c1ddd0349ee4577190c3de14343d81e9e2fd4
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-15 16:17:16 +0300

    add animated to hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,11 @@ const Hero = () => {
   return (
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-        <p>GROWING WITH DATA ANALYTICS</p>
+        <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
+        <div>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, flexible financing for BTB, BTC, SASS</p>
+        </div>
       </div>
     </div>
   )
