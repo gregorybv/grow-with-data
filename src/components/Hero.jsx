5eb2ac800f557c91d3a5bb1e6ad0be108a4ca5fb
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-19 19:07:53 +0300

    new animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {TypeAnimation} from "react-type-animation";
 
 const Hero = () => {
   return (
@@ -7,10 +8,23 @@ const Hero = () => {
         <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
         <div className='flex justify-center items-center'>
-          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for BTB, BTC, SASS</p>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for <TypeAnimation
+            sequence={[
+              'BTB',
+              2000,
+              'BTC',
+              2000,
+              'SASS',
+              2000,
+            ]}
+            wrapper="div"
+            cursor={true}
+            repeat={Infinity}
+            style={{fontSize: '1em', paddingLeft: '5px'}}
+          /></p>
         </div>
         <p className='md:text-2xl text-xl font-bold text-gray-500'>
-          Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.
+          Monitor your data analytics to increase revenue for platforms.
         </p>
      <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
       </div>
